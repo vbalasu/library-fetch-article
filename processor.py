a18a41488dd4d4977e8d5dd2edc3d492987347bc
Vijay Balasubramaniam <vbalasu@gmail.com>
2022-04-29 17:56:11 -0700

    Added SQS queue-based functionality

--- processor.py
+++ processor.py
@@ -20,16 +20,57 @@ def get_state():
 def identify_unprocessed(state):
     return [s for s in state if 'result' not in s]
 
-def process(unprocessed):
-    import subprocess, os
-    for u in unprocessed:
-        search_string = u['search_string']
-        output = subprocess.run(['python', 'library-fetch-article.py', search_string], capture_output=True)
-        print(output.stdout, output.stderr)
-        assert b'/tmp/' in output.stdout
-        filename = output.stdout.decode().replace('\n', '')
-        import boto3
-        s3 = boto3.client('s3')
-        key = 'apps/wsj/data/' + filename.replace('/tmp/', '')
-        s3.upload_file(filename, 'cloudmatica', key)
+def process(message):
+    import subprocess
+    search_string = message['search_string']
+    output = subprocess.run(['python', 'library-fetch-article.py', search_string], capture_output=True)
+    print(output.stdout, output.stderr)
+    assert b'/tmp/' in output.stdout
+    filename = output.stdout.decode().replace('\n', '')
+    import boto3
+    s3 = boto3.client('s3')
+    key = 'apps/wsj/data/' + filename.replace('/tmp/', '')
+    s3.upload_file(filename, 'cloudmatica', key)
+    return True
+
+def send_message(message_body):
+    import boto3
+    session = boto3.Session(profile_name='vbalasu_admin')
+    sqs = session.client('sqs')
+    queue_url = 'https://sqs.us-east-1.amazonaws.com/251566558623/library-fetch-article'
+    import json
+    response = sqs.send_message(QueueUrl=queue_url, MessageBody=json.dumps(message_body))
+    return response['ResponseMetadata']['HTTPStatusCode'] == 200
+
+def get_message():
+    import boto3
+    session = boto3.Session(profile_name='vbalasu_admin')
+    sqs = session.client('sqs')
+    queue_url = 'https://sqs.us-east-1.amazonaws.com/251566558623/library-fetch-article'
+    response = sqs.receive_message(QueueUrl=queue_url, WaitTimeSeconds=20)
+    if 'Messages' in response:
+        return response['Messages'][0]
+    else:
+        return False
+
+def delete_message(receipt_handle):
+    import boto3
+    session = boto3.Session(profile_name='vbalasu_admin')
+    sqs = session.client('sqs')
+    queue_url = 'https://sqs.us-east-1.amazonaws.com/251566558623/library-fetch-article'
+    response = sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=receipt_handle)
+    return response['ResponseMetadata']['HTTPStatusCode'] == 200
+    
+
+def loop():
+    import json
+    for ctr in range(2):
+        print(ctr)
+        message = get_message()
+        if message:
+            print(message['Body'])
+            process(json.loads(message['Body']))
+            delete_message(message['ReceiptHandle'])
+        else:
+            print('No message')
     return True
--- test_processor.py
+++ test_processor.py
@@ -14,6 +14,28 @@ def test_identify_processed():
     unprocessed = processor.identify_unprocessed(state)
     assert unprocessed == []
 
-def test_process():
-    result = processor.process([{"search_string": "Google", "who": "vbalasu@gmail.com"}])
+# def test_process():
+#     result = processor.process({"search_string": "Google", "who": "vbalasu@gmail.com"})
+#     assert result is True
+
+def test_send_message():
+    import datetime
+    result = processor.send_message({'time': datetime.datetime.utcnow().isoformat()})
+    assert result is True
+
+def test_get_and_delete_message():
+    result_get = processor.get_message()
+    assert type(result_get) == dict or result_get is False
+    if result_get:
+        result_delete = processor.delete_message(result_get['ReceiptHandle'])
+    assert result_delete is True
+
+def test_send_2_messages():
+    search_strings = ['Ukraine', 'Google Cloud', 'Databricks', 'Solana', 'Bitcoin']
+    for ctr in range(2):
+        processor.send_message({'ctr': ctr, 'search_string': search_strings[ctr]})
+    assert True
+
+def test_loop():
+    result = processor.loop()
     assert result is True
